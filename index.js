93cd34c5c482d195ed610a203b35d9c44314fe68
Ryan Guthrie <ryanguthrie@zoho.com>
2021-08-31 12:04:31 +0100

    commit tree 5338e32feccdcc6ecc179c6b2c1ed1bb82419174

--- index.js
+++ index.js
@@ -1,28 +1,34 @@
 const dodger = document.getElementById("dodger")
+
+dodger.style.backgroundColor = "#FF69B4"
+dodger.style.bottom = "0px"
+
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace("px"," ")
+  const left = parseInt(leftNumbers, 10)
+
+  if (left< 360) {
+    dodger.style.left = `${left + 1}px`
+  }
+}
+
 function moveDodgerLeft() {
-    const leftNumbers = dodger.style.left.replace("px", "");
-    const left = parseInt(leftNumbers, 10);
-  
+  const leftNumbers = dodger.style.left.replace("px"," ")
+    const left = parseInt(leftNumbers, 10)
+
     if (left > 0) {
-      dodger.style.left = `${left - 1}px`;
+      dodger.style.left = `${left - 1}px`
     }
+}
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "ArrowLeft") {
+    moveDodgerLeft()   
+}
+});
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "ArrowRight") {
+    moveDodgerRight()
   }
-  document.addEventListener("keydown", function(e) {
-    if (e.key === "ArrowLeft") {
-      moveDodgerLeft();
-    }
-  });
-  
-  function moveDodgerRight() {
-    const leftNumbers = dodger.style.left.replace("px", "");
-    const left = parseInt(leftNumbers, 10);
-  
-    if (left < 360) {
-      dodger.style.left = `${left + 1}px`;
-    }
-  }
-  document.addEventListener("keydown", function(e) {
-    if (e.key === "ArrowRight") {
-      moveDodgerRight();
-    }
-  });
+})
